6b722913512c66aec032a39ab6e82847683394dc
Adrien Djebar <26022@student.dorset-college.ie>
2022-10-04 09:24:21 +0100

    First Commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//4th Oct 2022 : Coding pratice 
+namespace OOP_Course5
+{
+    class Program
+    {
+
+    }
+}
